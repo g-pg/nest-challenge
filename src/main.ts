dbef9f9ef35e935aa76343314072959ddd792df3
Gabriel Gusso <gabrielpgusso@gmail.com>
2025-05-13 19:58:26 -0300

    add port env

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, () => console.log(`Server running on ${port}`));
 }
 bootstrap();
